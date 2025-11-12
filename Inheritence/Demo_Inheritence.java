0b4972c2c7234763b7566ce8f003e827585d6513
N Ajithchandra <157281543+AJITHCHANDRA-07@users.noreply.github.com>
2025-11-12 18:23:31 +0530

    Update Demo_Inheritence.java

--- Inheritence/Demo_Inheritence.java
+++ Inheritence/Demo_Inheritence.java
@@ -1,20 +1,22 @@
+//Deriving a Public class and method
 class Animal{
 	void sound() {
 		System.out.println("This is Animal class");
 	}
 }
-class Dog extends Animal{
+class Dog extends Animal{   // Inherit properties and features from parent class //child class (*Single inheritence)
 	void sound1() {
-		System.out.println("Dog Barks");
+		System.out.println("Dog Barks"); 
 	}
 }
-class Cat extends Animal{
+class Cat extends Animal{  // Inherit properties and features from parent class //child class (*Multiple inheritence)
 	void sound2() {
 		System.out.println("Cats meows");
 	}
 }
-class Cow extends Animal{
+class Cow extends Animal{  // Inherit properties and features from parent class //child class (*Multiple inheritence)
 	void sound3() {
 		System.out.println("Cow Moos");
 	}
+
 }

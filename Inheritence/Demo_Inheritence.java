1057f5996a6d73f8620af68a333245ac45c89b97
N Ajithchandra <157281543+AJITHCHANDRA-07@users.noreply.github.com>
2025-11-12 22:07:58 +0530

    Update Demo_Inheritence.java

--- Inheritence/Demo_Inheritence.java
+++ Inheritence/Demo_Inheritence.java
@@ -20,3 +20,35 @@ class Cow extends Animal{  // Inherit properties and features from parent class 
 	}
 
 }
+class Vehicle {   					// multilevel inheritence
+    Vehicle() {
+        System.out.println("This is a Vehicle");
+    }
+}
+class FourWheeler extends Vehicle {
+    FourWheeler() {
+        System.out.println("4 Wheeler Vehicles");
+    }
+}
+class Car extends FourWheeler {
+    Car() {
+        System.out.println("This 4 Wheeler Vehicle is a Car");
+    }
+}
+class Vehicle {   // Hierarchical Inheritance
+    Vehicle() {
+        System.out.println("This is a Vehicle");
+    }
+}
+
+class Car extends Vehicle {
+    Car() {
+        System.out.println("This Vehicle is Car");
+    }
+}
+
+class Bus extends Vehicle {
+    Bus() {
+        System.out.println("This Vehicle is Bus");
+    }
+}

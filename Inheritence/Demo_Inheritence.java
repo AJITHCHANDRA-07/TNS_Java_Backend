afd2e965a9ffcfb5370adf9ee1efa3e25811c237
N Ajithchandra <157281543+AJITHCHANDRA-07@users.noreply.github.com>
2025-11-12 16:38:02 +0530

    Add files via upload

---
+++ Inheritence/Demo_Inheritence.java
@@ -0,0 +1,20 @@
+class Animal{
+	void sound() {
+		System.out.println("This is Animal class");
+	}
+}
+class Dog extends Animal{
+	void sound1() {
+		System.out.println("Dog Barks");
+	}
+}
+class Cat extends Animal{
+	void sound2() {
+		System.out.println("Cats meows");
+	}
+}
+class Cow extends Animal{
+	void sound3() {
+		System.out.println("Cow Moos");
+	}
+}
---
+++ Inheritence/Main_Inheritence.java
@@ -0,0 +1,15 @@
+
+public class Main_Inheritence {
+
+	public static void main(String[] args) {
+		Cat a=new Cat();
+		a.sound();
+		a.sound2();
+		Cow b=new Cow();
+		b.sound3();
+		Dog c=new Dog();
+		c.sound1();
+		
+	}
+
+}
